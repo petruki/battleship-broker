37fb8993f1c86900c3f055943210e09dc7b2fd80
petruki <31597636+petruki@users.noreply.github.com>
2021-02-03 21:12:08 -0800

    Added match interation

--- src/broker-server.js
+++ src/broker-server.js
@@ -13,7 +13,9 @@ class BrokerServer {
             socket.on('CHECK_CREATE_ROOM', (req) => this.onCheckExistingRoom(socket, req));
             socket.on('PLAYER_HAS_JOINED', (req) => this.onPlayerJoinRoom(socket, req));
             socket.on('PLAYER_HAS_DISCONNECTED', (req) => this.onPlayerDisconnected(socket, req));
+            socket.on('PLAYER_HAS_SHOT', (req) => this.onPlayerShot(socket, req));
             socket.on('MATCH_STARTED', (req) => this.onMatchStarted(socket, req));
+            socket.on('NEW_ROUND', (req) => this.onNewRound(socket, req));
         });
     }
 
@@ -46,7 +48,7 @@ class BrokerServer {
         const player = removePlayer(socket.id);
 
         if (player) {
-            const playersInRoom = getPlayersInRoom(brokerMessage.room);
+            const playersInRoom = getPlayersInRoom(player.room);
             brokerMessage.players = playersInRoom;
             brokerMessage.message = `${player.username} has disconnected`;
             this.io.to(player.room).emit('ROOM_DATA', brokerMessage);
@@ -60,6 +62,34 @@ class BrokerServer {
     onMatchStarted(socket, req) {
         const host = getPlayer(socket.id);
         this.io.to(host.room).emit('MATCH_STARTED');
+    }
+
+    onNewRound(socket, req) {
+        const brokerMessage = JSON.parse(req);
+        const player = getPlayer(socket.id);
+        this.io.to(player.room).emit('NEW_ROUND', brokerMessage);
+
+        const playersInRoom = getPlayersInRoom(player.room);
+        playersInRoom.sort(() => Math.random() - 0.5);
+        this.io.to(playersInRoom[0].id).emit('SELECT_PLAYER');
+    }
+    
+    onPlayerShot(socket, req) {
+        const brokerMessage = JSON.parse(req);
+        const player = getPlayer(socket.id);
+
+        // update players board
+        this.io.to(player.room).emit('PLAYER_HAS_SHOT', brokerMessage);
+
+        // update rotation
+        const playersInRoom = getPlayersInRoom(player.room);
+        const playerIndex = playersInRoom.findIndex((p) => p.id === player.id);
+        
+        if (playerIndex + 1 < playersInRoom.length) {
+            this.io.to(playersInRoom[playerIndex + 1].id).emit('SELECT_PLAYER');
+        } else {
+            this.io.to(playersInRoom[0].id).emit('SELECT_PLAYER');
+        }
     }
 }
 
