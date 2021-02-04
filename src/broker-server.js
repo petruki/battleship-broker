b414314ed554f132274fc0fcdad6f4375a1d418c
petruki <31597636+petruki@users.noreply.github.com>
2021-02-04 12:38:15 -0800

    Added server log to the broker handlers

--- src/broker-server.js
+++ src/broker-server.js
@@ -20,79 +20,107 @@ class BrokerServer {
     }
 
     onCheckExistingRoom(socket, req) {
-        const brokerMessage = JSON.parse(req);
-        brokerMessage.exist = getRooms().includes(brokerMessage.room);
-
-        if (!brokerMessage.exist) {
-            addRoom(brokerMessage.room);
-            addPlayer({ id: socket.id, username: brokerMessage.player, room: brokerMessage.room, host: true });
-            socket.join(brokerMessage.room);
+        try {
+            const brokerMessage = JSON.parse(req);
+            brokerMessage.exist = getRooms().includes(brokerMessage.room);
+    
+            if (!brokerMessage.exist) {
+                addRoom(brokerMessage.room);
+                addPlayer({ id: socket.id, username: brokerMessage.player, room: brokerMessage.room, host: true });
+                socket.join(brokerMessage.room);
+            }
+            
+            this.io.to(socket.id).emit(brokerMessage.action, brokerMessage);
+        } catch (e) {
+            console.log(e);
         }
-        
-        this.io.to(socket.id).emit(brokerMessage.action, brokerMessage);
     }
 
     onPlayerJoinRoom(socket, req) {
-        const brokerMessage = JSON.parse(req);
-        socket.join(brokerMessage.room);
-        addPlayer({ id: socket.id, username: brokerMessage.player, room: brokerMessage.room, host: false });
-
-        socket.broadcast.to(brokerMessage.room).emit(brokerMessage.action, req);
-        brokerMessage.players = getPlayersInRoom(brokerMessage.room);
-        brokerMessage.message = `${brokerMessage.player} has joined`;
-        this.io.to(brokerMessage.room).emit('ROOM_DATA', brokerMessage);
+        try {
+            const brokerMessage = JSON.parse(req);
+            socket.join(brokerMessage.room);
+            addPlayer({ id: socket.id, username: brokerMessage.player, room: brokerMessage.room, host: false });
+    
+            socket.broadcast.to(brokerMessage.room).emit(brokerMessage.action, req);
+            brokerMessage.players = getPlayersInRoom(brokerMessage.room);
+            brokerMessage.message = `${brokerMessage.player} has joined`;
+            this.io.to(brokerMessage.room).emit('ROOM_DATA', brokerMessage);
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     onPlayerDisconnected(socket, req) {
-        const brokerMessage = JSON.parse(req);
-        const player = removePlayer(socket.id);
-
-        if (player) {
-            const playersInRoom = getPlayersInRoom(player.room);
-            brokerMessage.players = playersInRoom;
-            brokerMessage.message = `${player.username} has disconnected`;
-            this.io.to(player.room).emit('ROOM_DATA', brokerMessage);
-
-            if (!playersInRoom.length) {
-                removeRoom(player.room);
-            } else {
-                this.updateRotation(player);
+        try {
+            const brokerMessage = JSON.parse(req);
+            const player = removePlayer(socket.id);
+    
+            if (player) {
+                const playersInRoom = getPlayersInRoom(player.room);
+                brokerMessage.players = playersInRoom;
+                brokerMessage.message = `${player.username} has disconnected`;
+                this.io.to(player.room).emit('ROOM_DATA', brokerMessage);
+    
+                if (!playersInRoom.length) {
+                    removeRoom(player.room);
+                } else {
+                    this.updateRotation(player);
+                }
             }
+        } catch (e) {
+            console.log(e);
         }
     }
 
     onMatchStarted(socket, req) {
-        const host = getPlayer(socket.id);
-        this.io.to(host.room).emit('MATCH_STARTED');
+        try {
+            const host = getPlayer(socket.id);
+            this.io.to(host.room).emit('MATCH_STARTED');
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     onNewRound(socket, req) {
-        const brokerMessage = JSON.parse(req);
-        const player = getPlayer(socket.id);
-        this.io.to(player.room).emit('NEW_ROUND', brokerMessage);
+        try {
+            const brokerMessage = JSON.parse(req);
+            const player = getPlayer(socket.id);
+            this.io.to(player.room).emit('NEW_ROUND', brokerMessage);
 
-        const playersInRoom = getPlayersInRoom(player.room);
-        playersInRoom.sort(() => Math.random() - 0.5);
-        this.io.to(playersInRoom[0].id).emit('SELECT_PLAYER');
+            const playersInRoom = getPlayersInRoom(player.room);
+            playersInRoom.sort(() => Math.random() - 0.5);
+            this.io.to(playersInRoom[0].id).emit('SELECT_PLAYER');
+        } catch (e) {
+            console.log(e);
+        }
     }
     
     onPlayerShot(socket, req) {
-        const brokerMessage = JSON.parse(req);
-        const player = getPlayer(socket.id);
+        try {
+            const brokerMessage = JSON.parse(req);
+            const player = getPlayer(socket.id);
 
-        // update players board
-        this.io.to(player.room).emit('PLAYER_HAS_SHOT', brokerMessage);
-        this.updateRotation(player);
+            // update players board
+            this.io.to(player.room).emit('PLAYER_HAS_SHOT', brokerMessage);
+            this.updateRotation(player);
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     updateRotation(player) {
-        const playersInRoom = getPlayersInRoom(player.room);
-        const playerIndex = playersInRoom.findIndex((p) => p.id === player.id);
-        
-        if (playerIndex + 1 < playersInRoom.length) {
-            this.io.to(playersInRoom[playerIndex + 1].id).emit('SELECT_PLAYER');
-        } else {
-            this.io.to(playersInRoom[0].id).emit('SELECT_PLAYER');
+        try {
+            const playersInRoom = getPlayersInRoom(player.room);
+            const playerIndex = playersInRoom.findIndex((p) => p.id === player.id);
+            
+            if (playerIndex + 1 < playersInRoom.length) {
+                this.io.to(playersInRoom[playerIndex + 1].id).emit('SELECT_PLAYER');
+            } else {
+                this.io.to(playersInRoom[0].id).emit('SELECT_PLAYER');
+            }
+        } catch (e) {
+            console.log(e);
         }
     }
 }
